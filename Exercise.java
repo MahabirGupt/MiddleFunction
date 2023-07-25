b69b6debef083a1e79440c9f95a5a32c8b00a492
Mahabir <mahabirg81@gmail.com>
2023-07-25 20:55:46 +0800

    complete middle function exercise

--- Exercise.java
+++ Exercise.java
@@ -23,7 +23,7 @@ public class Exercise {
 
 
     public static void main(String[] args) {
-        int[] array =  new int[]{1, 2, 3};
+        int[] array =  new int[]{1, 2, 3,4};
         Exercise exercise = new Exercise( );
         exercise.middle(array);
 
